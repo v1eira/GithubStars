0bddb4819770c8743f792831a21d356d75073f30
v1eira <ewersonv@gmail.com>
2019-08-28 23:19:58 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# GithubStars <img src="android/app/src/main/res/mipmap-mdpi/ic_launcher_round.png" height="30px"/>
+
+An app that shows starred repositories by GitHub users and lets you browse through them.
+
+This project was made as a challenge on Rocketseat's 6th Bootcamp module.
+
+## Environment
+- OS: Ubuntu 18.04
+- Text editor: VS Code 1.37.0
+- Android emulator: Android Studio
+
+## Tools
+- Axios
+- React Native
+- ESLint
+- Prettier
+- Webview
+
+## Running the app:
+Clone this repository and, inside the project folder, run:
+```bash
+$ yarn
+```
+
+It'll install the necessary dependencies.
+
+To run it for the first time, do:
+```bash
+$ react-native run-android
+```
+
+If you have already ran the app before, just run:
+```bash
+$ react-native start
+```
+
+## Screens
+<p align="center">
+    <img src="examples/Main.png"/>
+</p>
+
+<p align="center">
+    <img src="examples/User.png"/>
+</p>
+
+<p align="center">
+    <img src="examples/Webview.png"/>
+</p>
---
+++ src/pages/User/index.js
@@ -0,0 +1,122 @@
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable react/static-property-placement */
+import React from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+  Loading,
+} from './styles';
+
+export default class User extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      stars: [],
+      loading: true,
+      page: 1,
+      refreshing: false,
+    };
+  }
+
+  async componentDidMount() {
+    this.load();
+  }
+
+  load = async (page = 1) => {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const { stars } = this.state;
+
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    });
+
+    this.setState({
+      stars: page > 1 ? [...stars, ...response.data] : response.data,
+      loading: false,
+      page,
+      refreshing: false,
+    });
+  };
+
+  loadMore = () => {
+    const { page } = this.state;
+
+    const nextPage = page + 1;
+
+    this.load(nextPage);
+  };
+
+  refreshList = () => {
+    this.setState({ refreshing: true, stars: [] }, this.load);
+  };
+
+  handleNavigate = repository => {
+    const { navigation } = this.props;
+
+    navigation.navigate('Repository', { repository });
+  };
+
+  render() {
+    const { navigation } = this.props;
+    const { stars, loading, refreshing } = this.state;
+
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        {loading ? (
+          <Loading />
+        ) : (
+          <Stars
+            data={stars}
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred onPress={() => this.handleNavigate(item)}>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
+      </Container>
+    );
+  }
+}
